6c7a4ba273eb6021d9b3422c0d2d9f4a7f2be5a5
ingyuseo <ghn06130@gmail.com>
2023-05-22 00:08:16 +0900

    second

--- src/App.js
+++ src/App.js
@@ -64,9 +64,32 @@ function MAIN(props) {
     const container = document.getElementById('map');
     const options = {
       center: new window.kakao.maps.LatLng(37.29713945326111, 126.971524553),
-      level: 3
+      level: 4
     };
-    new window.kakao.maps.Map(container, options);
+    var map = new window.kakao.maps.Map(container, options);
+
+
+    var geocoder = new window.kakao.maps.services.Geocoder();
+
+    geocoder.addressSearch('경기 수원시 장안구 서부로2105번길 26-3 1층', function(result, status) {
+
+      // 정상적으로 검색이 완료됐으면 
+       if (status === window.kakao.maps.services.Status.OK) {
+          var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+  
+          // 결과값으로 받은 위치를 마커로 표시합니다
+          var marker = new window.kakao.maps.Marker({
+              map: map,
+              position: coords
+          });
+          
+          marker.setMap(map);
+
+  
+          //map.setCenter(coords);
+      } 
+  });   
+
     
   }, []);
 
